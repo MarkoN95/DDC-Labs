cda0afed04619e334f151d5ec491f3926579b6f7
Marko Nikic <nikicm@student.ethz.ch>
2018-05-02 14:17:13 +0200

    change function name from diff to diff_abs

--- lab_7/asm/sad_color.asm
+++ lab_7/asm/sad_color.asm
@@ -46,21 +46,21 @@ abs_diff_color:
 	sw $a1, 4($sp)
 	sw $ra, 0($sp)
 	add $a1, $a3, $0	       # load R2 into $a1 (R1 already in $a0)
-	jal diff
+	jal diff_abs
 	add $t0, $v0, $0         # $t0 = diff(R1, R2)
 	lw $a0, 4($sp)		       # load G1 into $a0
 	lw $a1, 12($sp)		       # load G2 into $a1
-	jal diff
+	jal diff_abs
 	add $t0, $v0, $t0        # $t0 = diff(R1, R2) + diff(G1, G2)
 	add $a0, $a2, $0	       # load B1 into $a0
 	lw $a1, 8($sp)		       # load B2 into $a1
-	jal diff
+	jal diff_abs
 	add $v0, $t0, $v0        # $v0 = diff(R1, R2) + diff(G1, G2) + diff(B1, B2)
 	lw $ra, 0($sp)           # restore old $ra
 	add $sp, $sp 8           # free stack space
 	jr $ra
 
-diff:
+diff_abs:
 	sub $t1, $a0, $a1
 	sra $t2, $t1, 31
 	xor $t1, $t1, $t2
